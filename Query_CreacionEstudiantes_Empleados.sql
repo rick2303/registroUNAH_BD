5e5a8ef84cbc0000f5623181ae4bb744d9662dde
Rick <76531295+rick2303@users.noreply.github.com>
2023-06-23 17:47:31 -0600

    Update Query_CreacionEstudiantes_Empleados.sql

--- Query_CreacionEstudiantes_Empleados.sql
+++ Query_CreacionEstudiantes_Empleados.sql
@@ -6,39 +6,42 @@ CREATE TABLE estudiantes (
     NumeroTelefono VARCHAR(13),
     CorreoInstitucional VARCHAR(50),
     CorreoPersonal VARCHAR(50) not null,
-    Constrasena VARCHAR(100),
+    Contrasena VARCHAR(100),
     FechaNacimiento date,
-    Id_Carrera int not null,
+    Carrera VARCHAR(150) not null, 
     Direccion VARCHAR(200),
     IndiceGlobal float,
     IndicePeriodo float,
-    ID_CentroRegional int not null,
-    PuntajePAA int
+    CentroRegional VARCHAR(100) not null,
+    PuntajePAA int,
+    CONSTRAINT CK_estudiantes_CentroRegional CHECK (CentroRegional in ('CU', 'VS', 'CURC', 'CURNO', 'CURLA', 'CURLP', 'TEC-DANLI', 'TEC-AGUAN'))
 
 )
 
 CREATE TABLE empleados (
-    NumEmpleado VARCHAR(50) primary key,
+    NumEmpleado int IDENTITY(1,1) primary key,
     DNI VARCHAR(50) not null,
     Nombre VARCHAR (50) not null,
     Apellido VARCHAR(50) not null,
     NumeroTelefono VARCHAR(13) not null,
     CorreoInstitucional VARCHAR(50),
     CorreoPersonal VARCHAR(50) not null,
-    Constrasena VARCHAR(100),
+    Contrasena VARCHAR(100),
     FechaNacimiento date, /*opcional*/
     FechaContratacion date,
-    Id_Carrera int not null, 
+    Carrera VARCHAR(150), 
     Direccion VARCHAR(200),
     Foto VARCHAR(100),
-    ID_CentroRegional int not null,
+    CentroRegional VARCHAR(100) not null,
     Rol VARCHAR(30),
     SubRol VARCHAR(30),
-    CONSTRAINT CK_empleados_Rol CHECK(Rol in ('Admin', 'Docente')),
-    CONSTRAINT CK_empleados_SubRol CHECK (SubRol in ('Jefe Departamento', 'Coordinador', 'Docente'))
+    CONSTRAINT CK_empleados_Rol CHECK(Rol in ('ADMIN', 'DOCENTE')),
+    CONSTRAINT CK_empleados_SubRol CHECK (SubRol in ('JEFE DEPARTAMENTO', 'COORDINADOR', 'DOCENTE')),
+    CONSTRAINT CK_empleados_CentroRegional CHECK (CentroRegional in ('CU', 'VS', 'CURC', 'CURNO', 'CURLA', 'CURLP', 'TEC-DANLI', 'TEC-AGUAN'))
 
 )
 
 
 /*CK_empleados_Rol evalua que el rol a ingresar sean uno de esos dos
 CK_empleados_SubRol evalua si a su vez es Jefe de departamento, coordinador o docente sin mas*/
+
