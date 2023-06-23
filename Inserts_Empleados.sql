ee43314ef7514a6483acc7f47c2e95dd84d69546
Rick <76531295+rick2303@users.noreply.github.com>
2023-06-23 17:48:58 -0600

    Create Inserts_Empleados.sql

---
+++ Inserts_Empleados.sql
@@ -0,0 +1,42 @@
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
+    '1807195909001', 'Carlos Miguel', 'Rivera Martinez', '95321202', 'miguelitomartinez@unah.edu.hn', 'miguelitodelcarmen99ma@gmail.com',
+    'rauito23', '1959-06-23', GETDATE(), 'INGENIERIA ELECTRICA', 'Col. Sitrabarimasa', 'foto.jpg', 'TEC-AGUAN', 'DOCENTE', 'DOCENTE'
+)
+
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
+    '1807198009001', 'Abigail Valentina', 'Canales Nuñez', '95921202', 'cnlesabigail@unah.edu.hn', 'abigasvalentina@gmail.com',
+    'canales0322', '1980-06-23', GETDATE(), 'DERECHO', 'Col. Sitrabarimasa', 'foto1.jpg', 'CURNO', 'DOCENTE', 'DOCENTE'
+)
+
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
+    '1807198509001', 'Nestor Adrian', 'Ramirez Muñoz', '95122102', 'nestormunoz.edu.hn', 'munoznestor21@gmail.com',
+    'adrian802', '1985-06-23', GETDATE(), 'INGENIERIA EN SISTEMAS', 'Col. RES HONDURAS', 'foto2.jpg', 'CU', 'DOCENTE', 'JEFE DEPARTAMENTO'
+)
+
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
+    '1807199009001', 'Roberto Alejandro', 'Oseguera Castro', '85321202', 'robalejandro@unah.edu.hn', 'robseguera93@gmail.com',
+    'robertito72', '1990-06-23', GETDATE(), 'INGENIERIA INDUSTRIAL', 'Col. san miguel', 'foto3.jpg', 'VS', 'DOCENTE', 'COORDINADOR'
+)
+
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
+    '1807198909001', 'Julian Andres', 'Castro Alvarez', '95344202', 'jucastro89@unah.edu.hn', 'jalvarez3022@gmail.com',
+    'JulianAl75', '1989-06-23', GETDATE(), null, 'Col. Hato', 'foto5.jpg', 'CU', 'ADMIN', null
+)
+
+select * from empleados
+
