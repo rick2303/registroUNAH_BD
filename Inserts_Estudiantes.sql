9b1a1571b52d4a2968a529449b6c4e4dd11a1a51
Rick <76531295+rick2303@users.noreply.github.com>
2023-06-23 17:50:29 -0600

    Create Inserts_Estudiantes.sql

---
+++ Inserts_Estudiantes.sql
@@ -0,0 +1,43 @@
+insert into estudiantes (
+    NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
+    Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
+) values (
+    '20168003632', '1807199909001', 'Carlos Jesus', 'Raudales Martinez', '90234502', 'cjsrmartinez@unah.hn', 'raudales99ma@gmail.com',
+    'raudalito23', '1999-06-23', 'INGENIERIA ELECTRICA', 'Col. Sitrabarimasa', 80, 87, 'TEC-AGUAN', 777
+)
+
+insert into estudiantes (
+    NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
+    Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
+) values (
+    '20174003632', '0701200193822', 'Cristina Gabriela', 'Raudales Giron', '95264572', 'crisgiron@unah.hn', 'girongab21@gmail.com',
+    'crisgiron23', '2001-09-23', 'NUTRICION', 'Col. Las Aguas', 85, 87.22, 'CURNO', 1233
+)
+
+insert into estudiantes (
+    NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
+    Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
+) values (
+    '20181003632', '0801200009823', 'Francisco Javier', 'Morazan Mejia', '88542546', 'Franciscomejia20@unah.hn', 'rmorazanfranciso22@gmail.com',
+    'JAVlito23', '1999-06-23', 'INGENIERIA EN SISTEMAS', 'Col. Res Flores', 94, 67, 'CU', 1563
+)
+
+insert into estudiantes (
+    NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
+    Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
+) values (
+    '20215015435', '0801200593822', 'Andrea Gisselle', 'Zelaya Figueroa', '95226472', 'agisselle@unah.hn', 'gisselleAndrea21@gmail.com',
+    'agsz223a', '2005-09-23', 'DERECHO', 'Col. Las americas', 99, 87.22, 'CURLA', 1300
+)
+
+insert into estudiantes (
+    NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
+    Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
+) values (
+    '20235015435', '0801300793822', 'Andre Carlos', 'Acosta Figueroa', '95222212', 'acostacarlos@unah.hn', 'acosta07carlos@gmail.com',
+    'cardre2333', '2007-11-23', 'MEDICINA', 'Col. Las americas', 90, 82.43, 'CURC', 900
+)
+
+SELECT * from empleados
+select * from estudiantes
+
