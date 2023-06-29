a62ee3769c483bf830331a2e8d045be72bfd122f
Rick <76531295+rick2303@users.noreply.github.com>
2023-06-29 12:22:00 -0600

    Update InsertsClases.sql

--- InsertsClases.sql
+++ InsertsClases.sql
@@ -13,20 +13,23 @@ insert into clases values(
 
 
 insert into secciones (
-    IdClase, Edificio, Aula, CantidadAlumnos, HI, HF, Periodo, Fecha, IdDocente, Obs
+    IdClase, Edificio, Aula, CantidadAlumnos, HI, Seccion, HF, Periodo, Fecha, IdDocente, Obs
 ) values(
-    'IS-210', 'B2', '203', 20, '0900', '1000', '1PAC',  '2020-01-23', 15033, null
+    'IS-210', 'B2', '203', 20, '0900', 0900, '1000', '1PAC',  '2020-01-23', 15033, null
 ), (
-    'MM-110', 'F1', '307', 40, '1100', '1200', '1PAC', '2021-01-15', 15011, null
+    'MM-110', 'F1', '307', 40, '1100', 1100, '1200', '1PAC', '2021-01-15', 15011, null
 ), (
-    'IS-110', 'B2', '203', 20, '0900', '1000', '2PAC', '2021-05-15', 15044, null
+    'IS-110', 'B2', '203', 20, '0900', 0900,'1000', '2PAC', '2021-05-15', 15044, null
 ), (
-    'MM-111', 'F1', '308', 40, '0800', '0900', '3PAC', '2020-09-15', 15055, null
+    'MM-111', 'F1', '308', 40, '0800', 0800,'0900', '3PAC', '2020-09-15', 15055, null
 ), (
-    'SC-101', 'D1', '203', 30, '1200', '1300', '3PAC', '2022-09-17', 15022, null
+    'SC-101', 'D1', '203', 30, '1200', 1200, '1300', '3PAC', '2022-09-17', 15022, null
+), (
+    'SC-101', 'D1', '204', 30, '1200', 1201, '1300', '3PAC', '2022-09-17', 15055, null
 )
 
 select NumEmpleado, Carrera, rol, SubRol from empleados
 
 select * from secciones
 
+select * from secciones
