92a828a1723003ff816cfddc16a230f6f596ae9f
Rick <76531295+rick2303@users.noreply.github.com>
2023-06-28 21:06:13 -0600

    Create CreacionClases_Secciones.sql

---
+++ CreacionClases_Secciones.sql
@@ -0,0 +1,25 @@
+create table clases(
+    IdClase VARCHAR(20) primary key,
+    Nombre  VARCHAR(100) not NULL,
+    Requisitos VARCHAR(20),
+    UV INT not NULL
+)
+
+create table secciones(
+    IdSeccion int identity(1, 1) primary key,
+    IdClase VARCHAR(20),
+    Edificio VARCHAR(30),
+    Aula VARCHAR(30),
+    CantidadAlumnos int,
+    HI VARCHAR(20),
+    HF VARCHAR(20),
+    Periodo VARCHAR(10) not null,
+    Fecha DATE,
+    Obs VARCHAR(300),
+    IdDocente int,
+    CONSTRAINT Fk_secciones_empleados_id FOREIGN KEY (IdDocente) REFERENCES empleados(NumEmpleado) on delete CASCADE,
+    CONSTRAINT FK_secciones_clases_id FOREIGN KEY (IdClase) REFERENCES clases(IdClase) on DELETE CASCADE,
+    CONSTRAINT CK_secciones_periodo CHECK(Periodo in ('1PAC', '2PAC', '3PAC'))
+)
+
+
