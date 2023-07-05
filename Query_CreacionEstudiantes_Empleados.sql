65961073493f61490d1190ef5fc5226348ca8f45
Rick <76531295+rick2303@users.noreply.github.com>
2023-07-05 11:47:48 -0600

    Valores float en indices a int

--- Query_CreacionEstudiantes_Empleados.sql
+++ Query_CreacionEstudiantes_Empleados.sql
@@ -10,13 +10,16 @@ CREATE TABLE estudiantes (
     FechaNacimiento date,
     Carrera VARCHAR(150) not null, 
     Direccion VARCHAR(200),
-    IndiceGlobal float,
-    IndicePeriodo float,
+    IndiceGlobal int,
+    IndicePeriodo int,
     CentroRegional VARCHAR(100) not null,
     PuntajePAA int,
     CONSTRAINT CK_estudiantes_CentroRegional CHECK (CentroRegional in ('CU', 'VS', 'CURC', 'CURNO', 'CURLA', 'CURLP', 'TEC-DANLI', 'TEC-AGUAN'))
 
 )
+alter table estudiantes 
+add Estado VARCHAR(100) DEFAULT 'No matriculado'
+CONSTRAINT CK_estudiantes_estado CHECK (Estado in ('Matriculado', 'No matriculado', 'Penalizado'))
 
 CREATE TABLE empleados (
     NumEmpleado int IDENTITY(15000,11) primary key,
@@ -44,9 +47,14 @@ CREATE TABLE empleados (
 ALTER TABLE empleados
 ADD CONSTRAINT CK_empleados_dni UNIQUE(DNI);
 
+
+
+
 ALTER TABLE estudiantes
 ADD CONSTRAINT CK_estudiantes_dni UNIQUE(DNI);
 
 /*CK_empleados_Rol evalua que el rol a ingresar sean uno de esos dos
 CK_empleados_SubRol evalua si a su vez es Jefe de departamento, coordinador o docente sin mas*/
 
+
+select * from estudiantes
