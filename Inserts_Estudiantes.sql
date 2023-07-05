4c2469bb048321b3f7f795e7383c78dc632bc097
Rick <76531295+rick2303@users.noreply.github.com>
2023-07-05 11:48:54 -0600

    Update Inserts_Estudiantes.sql

--- Inserts_Estudiantes.sql
+++ Inserts_Estudiantes.sql
@@ -4,40 +4,55 @@ insert into estudiantes (
 ) values (
     '20168003632', '1807199909001', 'Carlos Jesus', 'Raudales Martinez', '90234502', 'cjsrmartinez@unah.hn', 'raudales99ma@gmail.com',
     'raudalito23', '1999-06-23', 'INGENIERIA ELECTRICA', 'Col. Sitrabarimasa', 80, 87, 'TEC-AGUAN', 777
-)
-
-insert into estudiantes (
-    NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
-    Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
-) values (
+),(
     '20174003632', '0701200193822', 'Cristina Gabriela', 'Raudales Giron', '95264572', 'crisgiron@unah.hn', 'girongab21@gmail.com',
-    'crisgiron23', '2001-09-23', 'NUTRICION', 'Col. Las Aguas', 85, 87.22, 'CURNO', 1233
-)
-
-insert into estudiantes (
-    NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
-    Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
-) values (
+    'crisgiron23', '2001-09-23', 'NUTRICION', 'Col. Las Aguas', 85, 87, 'CURNO', 1233
+),(
     '20181003632', '0801200009823', 'Francisco Javier', 'Morazan Mejia', '88542546', 'Franciscomejia20@unah.hn', 'rmorazanfranciso22@gmail.com',
     'JAVlito23', '1999-06-23', 'INGENIERIA EN SISTEMAS', 'Col. Res Flores', 94, 67, 'CU', 1563
-)
-
-insert into estudiantes (
-    NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
-    Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
-) values (
+), (
     '20215015435', '0801200593822', 'Andrea Gisselle', 'Zelaya Figueroa', '95226472', 'agisselle@unah.hn', 'gisselleAndrea21@gmail.com',
-    'agsz223a', '2005-09-23', 'DERECHO', 'Col. Las americas', 99, 87.22, 'CURLA', 1300
+    'agsz223a', '2005-09-23', 'DERECHO', 'Col. Las americas', 99, 87, 'CURLA', 1300
+),(
+    '20235015435', '0801300793822', 'Andre Carlos', 'Acosta Figueroa', '95222212', 'acostacarlos@unah.hn', 'acosta07carlos@gmail.com',
+    'cardre2333', '2007-11-23', 'MEDICINA', 'Col. Las americas', 90, 82, 'CURC', 900
 )
 
-insert into estudiantes (
+
+insert into estudiantes(
     NumCuenta, DNI ,Nombre, Apellido ,NumeroTelefono, CorreoInstitucional, CorreoPersonal,
     Contrasena, FechaNacimiento, Carrera, Direccion, IndiceGlobal, IndicePeriodo, CentroRegional, PuntajePAA
 ) values (
-    '20235015435', '0801300793822', 'Andre Carlos', 'Acosta Figueroa', '95222212', 'acostacarlos@unah.hn', 'acosta07carlos@gmail.com',
-    'cardre2333', '2007-11-23', 'MEDICINA', 'Col. Las americas', 90, 82.43, 'CURC', 900
+    '20212003222', '1807199209001', 'Andrea Nicolle', 'Raudales Martinez', '90234502', 'andreanicollez@unah.hn', 'raudalnicki9ma@gmail.com',
+    'raudalito23', '1999-06-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 80, 77, 'VS', 800
+),(
+    '2018201940', '0711200193822', 'Nicki Nicole', 'Garcia Do Santos', '92467572', 'nickinicole@unah.hn', 'nicolenickib21@gmail.com',
+    'random01', '2001-09-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 81, 78, 'VS', 800
+),(
+    '20192003632', '0801200109823', 'Cazzu', 'Morazan Mejia', '88542546', 'clementedurn541@unah.hn', 'rmorazanfranci2so22@gmail.com',
+    'random01', '1999-06-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 82, 79, 'VS', 800
+), (
+    '20212015435', '090120024224', 'Gissela Gisseru', 'Zelaya Figueroa', '95226472', 'marisolleiva55@unah.hn', 'marisolleiva55@gmail.com',
+    'random01', '2005-09-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 83, 80, 'VS', 800
+),(
+    '20222015435', '090120024225', 'Asuna Valentina', 'Acosta Figueroa', '95222212', 'germncarmona_casares@unah.hn', 'germncarmona_casares@gmail.com',
+    'random01', '2007-11-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 84, 81, 'VS', 800
+), (
+    '20212003112', '090120024221', 'Charles Dawrwin', 'Chapin Martinez', '90234502', 'sanchohenrquez@unah.hn', 'sanchohenrquez@gmail.com',
+    'random01', '1999-06-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 85, 82, 'VS', 800
+),(
+    '20172024322', '110120024224', 'Cristina Gabriela', 'Gamez Giron', '95264572', 'mateotijerina_rivera@unah.hn', 'mateotijerina_rivera@gmail.com',
+    'random21', '2001-09-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 86, 83, 'VS', 800
+),(
+    '20201104222', '090120014224', 'Fransheka', 'Morazan Ruiz', '88542540', 'clementedurn54@unah.hn', 'clementedurn54@gmail.com',
+    'JAVlito23', '1999-06-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 87, 84, 'VS', 800
+), (
+    '20212032445', '120120024224', 'Dua Abigail', 'Zelaya Garcia', '95226402', 'caridaddelao72@unah.hn', 'caridaddelao72@gmail.com',
+    'agsz223a', '2005-09-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 88, 85, 'VS', 800
+),(
+    '20221015755', '1609200193822', 'Carlos Jose', 'Acosta Figueroa', '95200212', 'maradelcarmenbueno_ortiz57@unah.hn', 'maradelcarmenbueno_ortiz57@gmail.com',
+    'cardre2333', '2007-11-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 89, 86, 'VS', 800
+), (
+    '20182032832', '110120024254', 'Diego Jose', 'Bustillo Martinez', '73483292', 'estebanfigueroa@unah.hn', 'estebanfigueroa@gmail.com',
+    'raudalito23', '1999-06-23', 'Ingenieria industrial', 'Col. Sitrabarimasa', 100, 87, 'VS', 800
 )
-
-SELECT * from empleados
-select * from estudiantes
-
