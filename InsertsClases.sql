f072c84fa88053f677c977e9eb3be725e273b3f3
Rick <76531295+rick2303@users.noreply.github.com>
2023-07-10 00:10:13 -0600

    Update InsertsClases.sql

--- InsertsClases.sql
+++ InsertsClases.sql
@@ -1,5 +1,10 @@
-
 insert into clases values(
+    'MM-202', 'Cálculo 2 ', 'MM-201', 5 , 'Matematica pura'
+),(
+    'MM-411', 'Ecuaciones Diferenciales', 'MM-202', 3, 'Matematica pura'
+),(
+    'FS-100', 'Fisica General', 'MM-201/MM211', 5, 'Fisica'
+), (
     'IS-210', 'Programacion II', 'MM-314', 4, 'Ingenieria en sistemas'
 ), (
     'IS-110', 'Introduccion a la ingenieria en sistemas', 'Ninguno', 3, 'Ingenieria en sistemas'
@@ -85,3 +90,80 @@ insert into clases values(
     'NUT-113', 'Socioeconomía alimentaria nutricional', 'NUT-103', 4, 'Nutricion'
 )
 
+
+/*Clases de Plan de clases de ing en sistemas*/
+insert into clases values(
+    'IS-311', 'Cirucitos Electricos para ing. en sistemas', 'MM-411/FS-100', 3, 'Ingenieria en sistemas'
+), (
+    'IS-310', 'Algoritmos y Estructura de Datos', 'IS-210', 4, 'Ingenieria en sistemas'
+), (
+    'IS-410', 'Programación Orientada a Objetos', 'IS-310', 5, 'Ingenieria en sistemas'
+), (
+    'IS-411', 'Electrónica para Ing. en sistemas', 'IS-311', 3, 'Ingenieria en sistemas'
+), (
+    'IS-412', 'Sistemas Operativos I', 'IS-310/MM-420', 4, 'Ingenieria en sistemas'
+), (
+    'IS-501', 'Base de datos I', 'MM-401/IS-410', 5, 'Ingenieria en sistemas'
+), (
+    'IS-510', 'Instalacion Elétricas', 'IS-311',3, 'Ingenieria en sistemas'
+), (
+    'IS-511', 'Redes de datos I', 'IS-411', 4, 'Ingenieria en sistemas'
+), (
+    'IS-512', 'Sistemas Operativos II', 'IS-412', 5, 'Ingenieria en sistemas'
+), (
+    'IS-601', 'Base de datos II', 'IS-501', 4, 'Ingenieria en sistemas'
+)
+
+/*Clases de Plan de clases de ing industrial*/
+insert into clases values(
+    'II-111', 'Introduccion a la ing industrial', 'Ninguno', 2, 'Ingenieria industrial'
+),(
+    'II-211', 'Estadistica I', 'MM-202/II-111', 3, 'Ingenieria Industrial'
+),(
+    'II-221', 'Programación y computación', 'MM-202', 3, 'Ingenieria industrial'
+), (
+    'II-222', 'Estadistica Aplicada', 'II-221', 3, 'Ingenieria industrial'
+),(
+    'II-311', 'Instalaciones Eléctricas', 'FS-200', 5, 'Ingenieria industrial'
+), (
+    'II-231', 'Programación Aplicada', 'II-221', 3, 'Ingenieria industrial'
+), (
+    'II-232', 'Ingeniería de Métodos I', 'II-222', 4, 'Ingenieria industrial'
+), (
+    'II-321', 'Investigación de Operaciones I', 'II-231', 4, 'Ingenieria industrial'
+),(
+    'II-312', 'Ingeniería de Métodos II', 'II-232', 3, 'Ingenieria industrial'
+), (
+    'II-313', 'Contabilidad industrial I', 'II-222/II-231', 4, 'Ingenieria industrial'
+)
+
+/*Clases de Plan de clases de ing electrica*/
+insert into clases values(
+    'IE-210', 'Introduccion a la ing eléctrica', 'MM-201', 2, 'Ingenieria electrica'
+),(
+    'FS-200', 'Física general II', 'FS-100', 5, 'Fisica'
+),(
+    'IN-102', 'Ingles II', 'IN-101', 4, 'Lenguas extranjeras'
+), (
+    'FS-321', 'Electricidad y magnetismo', 'FS-200/MM-411', 5, 'Fisica'
+),(
+    'IE-221', 'Circuitos Eléctricos', 'FS-100/IE-210', 5, 'Ingenieria electrica'
+), (
+    'FS-415', 'Electricidad y magnetismo II', 'FS-321', 5, 'Fisica'
+), (
+    'IE-313', 'Metodos matemáticos para ingenieria', 'MM-411', 3, 'Ingenieria electrica'
+), (
+    'IE-311', 'Circuitos Eléctricos II', 'IE-221', 5, 'Ingenieria electrica'
+),(
+    'IE-421', 'Máquinas eléctricas', 'IE-311/IE-313', 4, 'Ingenieria electrica'
+), (
+    'MM-502', 'Variable compleja I', 'MM-202', 3, 'Ingenieria electrica'
+),(
+    'IE-416', 'Electromagenistmo I', 'FS-415/IE-313', 4, 'Ingenieria electrica'
+), (
+    'IE-314', 'Electrónica I', 'IE-311', 4, 'Ingenieria electrica'
+),(
+    'IE-423', 'Teoría de la probabilidad', 'MM-401/MM-411', 3, 'Ingenieria electrica'
+), (
+    'IE-511', 'Máquinas eléctricas II', 'IE-421/IE-416', 4, 'Ingenieria electrica'
+)
