a961ef069cb514e96ffa1ed6abe9a3f4b8b29e15
Rick <76531295+rick2303@users.noreply.github.com>
2023-07-10 00:09:23 -0600

    Update Inserts_Empleados.sql

--- Inserts_Empleados.sql
+++ Inserts_Empleados.sql
@@ -2,21 +2,46 @@ insert into empleados (
     DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
     FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
 ) VALUES (
-    '1807198909001', 'Julian Andres', 'Castro Alvarez', '95344202', 'jucastro89@unah.edu.hn', 'jalvarez3022@gmail.com',
-    'JulianAl75', '1989-06-23', GETDATE(), null, 'Col. Hato', 'foto5.jpg', 'CU', 'ADMIN', null
-),(
     '1808199929001', 'Samuel Joseph', 'Mendez Lozano', '95329302', 'samuelito99@unah.edu.hn', 'sammendez99@gmail.com',
     'sammendezjoseph01', '2019-06-23', GETDATE(), 'FISICA', 'Col. Sitrabarimasa', 'foto.jpg', 'CU', 'DOCENTE', 'DOCENTE'
-),(
+)
+
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
     '1807198009001', 'Abigail Valentina', 'Canales Nuñez', '95921202', 'cnlesabigail@unah.edu.hn', 'abigasvalentina@gmail.com',
     'canales0322', '1980-06-23', GETDATE(), 'DERECHO', 'Col. Sitrabarimasa', 'foto1.jpg', 'CURNO', 'DOCENTE', 'DOCENTE'
-),(
+)
+
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
     '1807198509001', 'Nestor Adrian', 'Ramirez Muñoz', '95122102', 'nestormunoz.edu.hn', 'munoznestor21@gmail.com',
     'adrian802', '1985-06-23', GETDATE(), 'INGENIERIA EN SISTEMAS', 'Col. RES HONDURAS', 'foto2.jpg', 'CU', 'DOCENTE', 'JEFE DEPARTAMENTO'
-),(
+)
+
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
     '1807199009001', 'Roberto Alejandro', 'Oseguera Castro', '85321202', 'robalejandro@unah.edu.hn', 'robseguera93@gmail.com',
     'robertito72', '1990-06-23', GETDATE(), 'INGENIERIA INDUSTRIAL', 'Col. san miguel', 'foto3.jpg', 'VS', 'DOCENTE', 'COORDINADOR'
-),  (
+)
+
+insert into empleados (
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
+    '1807198909001', 'Julian Andres', 'Castro Alvarez', '95344202', 'jucastro89@unah.edu.hn', 'jalvarez3022@gmail.com',
+    'JulianAl75', '1989-06-23', GETDATE(), null, 'Col. Hato', 'foto5.jpg', 'CU', 'ADMIN', null
+)
+
+insert into empleados(
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
     '0901194012345', 'Luis Miguel', 'Argueta Maradriaga', '89323423', 'luismigmaradriaga@unah.edu.hn', 'miguelitoluis@gmail.com',
     'random01', '1940-01-29', GETDATE(), 'Derecho', 'Col. Hato', 'foto6.jpg', 'CU', 'DOCENTE', 'DOCENTE'
 ),(
@@ -41,3 +66,45 @@ insert into empleados (
     '0901197512345', 'Ramon Andres', 'Amaya Amador', '90276372', 'amayaamador@unah.edu.hn', 'ramonamaya0801@gmail.com',
     'amigosenemigos', '1975-03-31', GETDATE(),  'Matematica', 'Ecovivienda', 'foto13.jpg', 'CU', 'DOCENTE', 'DOCENTE'
 );
+
+insert into empleados(
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
+    '1807198009902', 'Zelfa Elisa', 'Vasquez Maradriaga', '89373223', 'elisazelfa.edu.hn', 'zelfa911@gmail.com',
+    'random01', '1980-01-29', GETDATE(), 'Ingenieria en sistemas', 'Col. Hato', 'foto6.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+),(
+    '1807198109902', 'Zelda', 'Ocarina Artega', '89929492', 'zeldabestogame@unah.edu.hn', 'zeldaily63@gmail.com',
+    'dummie03', '1981-04-12', GETDATE(), 'Ingenieria en sistemas', 'Las brisas', 'foto7.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+),(
+    '1807198009904', 'Link De Hyrule', 'Pereira Alvarez', '97223891', 'linkheroe@unah.edu.hn', 'salvadordehyrule@gmail.com',
+    'datarandom3', '1980-09-20', GETDATE(), 'Ingenieria en sistemas', 'Col. San Luis', 'foto8.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+),(
+    '1807198109904', 'Dante Enrique', 'Carcamo Filipino', '83673829', 'ruizidkw@unah.edu.hn', 'danteinfern@gmail.com',
+    'migueeel3', '1981-06-27', GETDATE(), 'Ingenieria en sistemas', 'Kennedy', 'foto9.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+),(
+    '1807198009906', 'Enrique Jesus', 'Ruiz Alvarez', '95144122', 'juruizjesus@unah.edu.hn', 'juruizjesus2@gmail.com',
+    'pato', '1960-02-06', GETDATE(), 'Ingenieria industrial', 'Col. 24 de mayo', 'foto10.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+),(
+    '1807198109906', 'Ryu', 'Agurcia Garcia', '99926233', 'ryuagurcia@unah.edu.hn', 'ryuque022@gmail.com',
+    'avestruz', '1981-04-23', GETDATE(), 'Ingenieria industrial', 'Conquista', 'foto11.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+),(
+    '1807198009908', 'IBai', 'Llanos Martinez', '95344602', 'ibaillanos@unah.edu.hn', 'maquiavelo22@gmail.com',
+    'cerealconleche', '1970-03-23', GETDATE(),  'Ingenieria industrial', 'Col. San Pablo', 'foto12.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+),(
+    '1807198209908', 'Francisco Jesus', 'Morazan Amador', '90276372', 'morazanamador@unah.edu.hn', 'fmorazanca@gmail.com',
+    'amigosenemigos', '1975-03-31', GETDATE(),  'Ingenieria industrial', 'Ecovivienda', 'foto13.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+);
+
+select NumEmpleado, dni, Carrera, SubRol, contrasena, centroregional from empleados 
+
+insert into empleados(
+    DNI, Nombre,Apellido, NumeroTelefono, CorreoInstitucional, CorreoPersonal,Contrasena,FechaNacimiento,
+    FechaContratacion, Carrera, Direccion, Foto, CentroRegional, Rol, SubRol
+) VALUES (
+    '1801198009902', 'Susana Fabiola', 'Maradriaga Vasquez', '89373200', 'efabiola.maradriada@unah.edu.hn', 'susanavasquez@gmail.com',
+    'random01', '1980-01-29', GETDATE(), 'Ingenieria electrica', 'Col. Hato', 'foto6.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+),(
+    '1801198109902', 'Elizabeth', 'Maradriaga Artega', '89929400', 'elizabeth.maradriaga@unah.edu.hn', 'maradriagaElizabeth@gmail.com',
+    'dummie03', '1981-04-12', GETDATE(), 'Ingenieria electrica', 'Las brisas', 'foto7.jpg', 'CU', 'DOCENTE', 'DOCENTE'
+)
