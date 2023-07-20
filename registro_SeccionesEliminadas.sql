91b696e343e2e1d4d8ad02b374a1c1913aa633bc
Rick <76531295+rick2303@users.noreply.github.com>
2023-07-20 12:52:47 -0600

    Procedimiento almacenado para eliminar una seccion

--- registro_SeccionesEliminadas.sql
+++ registro_SeccionesEliminadas.sql
@@ -12,4 +12,25 @@ CREATE table registro_secciones_eliminadas(
     Justificacion VARCHAR(200),
     IdDocente int,
     CONSTRAINT CK_seccioneseliminadas_periodo CHECK(Periodo in ('1PAC', '2PAC', '3PAC'))
-)
+);
+
+CREATE PROCEDURE SP_EliminarSeccion
+    @IdSeccion INT,
+    @Justificacion VARCHAR(200),
+    @Asignatura VARCHAR(200),
+    @UV INT
+AS
+BEGIN
+    /* Guardar los datos en la tabla registro_secciones_eliminadas */
+    INSERT INTO registro_secciones_eliminadas (IdSeccion, IdClase, Asignatura, UV, Seccion, HI, HF, Año, Periodo, Justificacion, IdDocente)
+    SELECT IdSeccion, IdClase, @Asignatura, @UV, CAST(Seccion AS VARCHAR(10)), HI, HF, Fecha, Periodo, @Justificacion, IdDocente
+    FROM secciones
+    WHERE IdSeccion = @IdSeccion;
+
+    /* Eliminar la sección de la tabla secciones */
+    DELETE FROM secciones
+    WHERE IdSeccion = @IdSeccion;
+END;
+
+EXEC SP_EliminarSeccion @IdSeccion = 145, @Justificacion = 'Sección cancelada debido a baja demanda', @Asignatura = 'Programacion2', @UV = 4;
+
