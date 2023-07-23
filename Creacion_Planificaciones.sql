06e1e969d7b0826aed519e91a04c096942939b47
Rick <76531295+rick2303@users.noreply.github.com>
2023-07-23 16:00:26 -0600

    Update Creacion_Planificaciones.sql

--- Creacion_Planificaciones.sql
+++ Creacion_Planificaciones.sql
@@ -3,6 +3,7 @@ create TABLE planificacion_academica(
     FechaInicio DATE,
     FechaFinal DATE,
     PeriodoAcademico VARCHAR(20),
+    Sistema VARCHAR(100),
     CONSTRAINT CK_planificacionacademica_periodo CHECK (PeriodoAcademico in ('1PAC', '2PAC', '3PAC'))
 )
 
@@ -13,6 +14,7 @@ create TABLE planificacion_matricula(
     HoraInicio VARCHAR(12), 
     HoraFinal varchar(12),
     PeriodoAcademico VARCHAR(20),
+    Sistema VARCHAR(100),
     CONSTRAINT CK_planificacionmatricula_periodo CHECK (PeriodoAcademico in ('1PAC', '2PAC', '3PAC'))
 )
 
@@ -23,6 +25,28 @@ create TABLE planificacion_cancelacionesexcepcionales(
     HoraInicio VARCHAR(12), 
     HoraFinal varchar(12),
     PeriodoAcademico VARCHAR(20),
+    Sistema VARCHAR(100),
     CONSTRAINT CK_planificacioncancelaciones_periodo CHECK (PeriodoAcademico in ('1PAC', '2PAC', '3PAC'))
 )
 
+create TABLE planificacion_ingresonotas(
+    idPlanificacion int IDENTITY(1 ,1) PRIMARY KEY,
+    FechaInicio DATE,
+    FechaFinal DATE,
+    HoraInicio VARCHAR(12), 
+    HoraFinal varchar(12),
+    PeriodoAcademico VARCHAR(20),
+    Sistema VARCHAR(100),
+    CONSTRAINT CK_planificacioningresonotas_periodo CHECK (PeriodoAcademico in ('1PAC', '2PAC', '3PAC'))
+)
+
+create TABLE planificacion_creacionsecciones(
+    idPlanificacion int IDENTITY(1 ,1) PRIMARY KEY,
+    FechaInicio DATE,
+    FechaFinal DATE,
+    HoraInicio VARCHAR(12), 
+    HoraFinal varchar(12),
+    PeriodoAcademico VARCHAR(20),
+    Sistema VARCHAR(100),
+    CONSTRAINT CK_planificacioncreacionsecciones_periodo CHECK (PeriodoAcademico in ('1PAC', '2PAC', '3PAC'))
+)
